ec084c7dd7b7f9dc10cb464b56104543928bf88e
Skulloh <764131601@qq.com>
2021-05-13 08:32:44 +0800

    第三次提交

---
+++ MrPotato/Assets/Scripts/BackGround.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackGround : MonoBehaviour
+{
+    public Transform[] backGround;
+    public Transform cam;
+    public float fparallax = 0.4f;
+    public float layerFraction = 5f;
+    public float fSmooth = 5f;
+    private Vector3 previousCamPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousCamPos = cam.position;
+    }
+
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fParrlaX = (previousCamPos.x - cam.position.x) * fparallax;
+        for (int i = 0; i < backGround.Length; i++)
+        {
+            float fNewX = backGround[i].position.x + fParrlaX * (1 + i * layerFraction);
+            Vector3 newPos = new Vector3(fNewX, backGround[i].position.y,backGround[i].position.z);
+            backGround[i].position = Vector3.Lerp(backGround[i].position,newPos,fSmooth * Time.deltaTime);
+        }
+
+        previousCamPos = cam.position;
+    }
+}
---
+++ MrPotato/Assets/Scripts/MCamera.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCamera : MonoBehaviour
+{
+    private Transform player;
+    public float MaxDistanceX = 2;
+    public float MaxDistanceY = 2;
+    private float SmoothX = 1;
+    private float SmoothY = 1;
+    public Vector2 MinCamXandY = new Vector2(-7,-7);
+    public Vector2 MaxCamXandY = new Vector2(9,9);
+    bool NeedMoveX()
+    {
+        return Mathf.Abs(transform.position.x - player.position.x) > MaxDistanceX;
+    }
+    bool NeedMoveY()
+    {	
+        return Mathf.Abs(transform.position.y - player.position.y) > MaxDistanceY;
+    }
+
+    void Start()
+    {
+
+    }
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    void Update()
+    {
+
+    }
+    
+    private void TrackPlayer()
+    {
+        float CamNewX = transform.position.x;
+        float CamNewY = transform.position.y;
+        if (NeedMoveX())
+            CamNewX = Mathf.Lerp(transform.position.x,player.position.x, SmoothX * Time.deltaTime);
+        if (NeedMoveY())
+            CamNewY = Mathf.Lerp(transform.position.y, player.position.y, SmoothY * Time.deltaTime);
+        
+        transform.position = new Vector3(CamNewX, CamNewY,transform.position.z);
+
+    }
+
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+}
+
